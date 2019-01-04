c1e5dfc59cfe9c7ca7448b18b5434b8b671f2da9
nem035 <n.stojanovic035@gmail.com>
2019-01-03 21:49:02 -0500

    :sparkles: add `img` child to our app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const vApp = createElement(
   {
     id: 'app',
   },
-  ['Hello World']
+  [
+    'Hello World',
+    createElement('img', {
+      src: 'https://media.giphy.com/media/l1IYk3l6xcEDN5RFS/giphy.gif',
+    }),
+  ]
 );
 
 const $app = render(vApp);
