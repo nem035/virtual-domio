1bfd328f42ec744e271cdb32984d79c463965f90
nem035 <n.stojanovic035@gmail.com>
2019-01-03 22:07:55 -0500

    :sparkles: add reactive state

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import { createElement, render } from './vdom';
 import mount from './vdom/mount';
 
-const vApp = createElement(
-  'div',
-  {
-    id: 'app',
-  },
-  [
-    'Hello World',
-    createElement('img', {
-      src: 'https://media.giphy.com/media/l1IYk3l6xcEDN5RFS/giphy.gif',
-    }),
-  ]
-);
+const createApp = ({ count }) =>
+  createElement(
+    'div',
+    {
+      id: 'app',
+      count: count,
+    },
+    [
+      String(count),
+      createElement('img', {
+        src: 'https://media.giphy.com/media/l1IYk3l6xcEDN5RFS/giphy.gif',
+      }),
+    ]
+  );
 
-const $app = render(vApp);
+const state = {
+  count: 0,
+};
 
-mount($app, document.querySelector('#app'));
+let $root = document.querySelector('#app');
+$root = mount(render(createApp(state)), $root);
+
+setInterval(() => {
+  console.log(`rendering count ${state.count}`);
+  $root = mount(render(createApp(state)), $root);
+  state.count += 1;
+}, 1000);
