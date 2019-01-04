9b10a01d2f3bd74979c3bf568bd7e20d508c301a
nem035 <n.stojanovic035@gmail.com>
2019-01-03 23:40:59 -0500

    :sparkles: add a more detailed demo

--- src/index.js
+++ src/index.js
@@ -8,10 +8,14 @@ const createApp = ({ count }) =>
       count: count,
     },
     [
-      String(count),
-      createElement('input', { type: 'text' }),
+      createElement('h1', undefined, ['VirtualDOMio']),
+      createElement('span', { id: 'count' }, [String(count)]),
+      createElement('input', {
+        type: 'text',
+        placeholder: 'Try typing while the app is updating',
+      }),
       createElement('img', {
-        src: 'https://media.giphy.com/media/l1IYk3l6xcEDN5RFS/giphy.gif',
+        src: 'https://media.giphy.com/media/26DNdzl2XkgbbFAmk/giphy.gif',
       }),
     ]
   );
@@ -24,9 +28,9 @@ let vApp = createApp(state);
 let $root = mount(render(vApp), document.querySelector('#app'));
 
 setInterval(() => {
+  state.count += 1;
   const newVApp = createApp(state);
   const patch = diff(vApp, newVApp);
   $root = patch($root);
   vApp = newVApp;
-  state.count += 1;
 }, 1000);
