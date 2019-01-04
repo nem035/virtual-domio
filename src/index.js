c4d073830567ebd4449b5dc4c7351842bf9531bd
nem035 <n.stojanovic035@gmail.com>
2019-01-03 21:14:58 -0500

    :sparkles: add `createElement`

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+import createElement from './vdom/createElement';
+
+const vApp = createElement('div', {
+  attrs: {
+    id: 'app'
+  },
+  children: [
+    'Hello World'
+  ]
+})
+
+console.log(vApp)
